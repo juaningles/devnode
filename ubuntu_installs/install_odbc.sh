cf9b08a7ab5900be84786cd63e85840dada11ab2
juaningles <157614936+juaningles@users.noreply.github.com>
2024-03-13 14:07:15 -0700

    Update install_odbc.sh

--- ubuntu_installs/install_odbc.sh
+++ ubuntu_installs/install_odbc.sh
@@ -6,7 +6,8 @@ apt-get -y install python3 python3-pip
 RUN apt-get update
 apt-get -y install gnupg2
 apt-get -y install apt-transport-https
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# previous method: curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 curl https://packages.microsoft.com/config/ubuntu/$(awk -F "=" '/^DISTRIB_RELEASE=/ {print $2}' /etc/lsb-release | sed -e 's/22\.10/22.04/')/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 ACCEPT_EULA=Y apt-get -y install msodbcsql18
