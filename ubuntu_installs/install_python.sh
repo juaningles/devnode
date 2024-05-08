e1d45c5865ee574e892bbba7da611a77c97c4e68
juan <juan@example.com>
2024-05-08 11:47:05 -0700

    add python3-venv

--- ubuntu_installs/install_python.sh
+++ ubuntu_installs/install_python.sh
@@ -5,5 +5,5 @@ set -e -x
 SUDO=$(which sudo |  head -n 1 | tr -d "\r\n")
 
 $SUDO apt-get update
-$SUDO apt-get -y install python3 python3-pip
+$SUDO apt-get -y install python3 python3-pip python3-venv
 $SUDO ln -s /usr/bin/python3 /usr/bin/python
