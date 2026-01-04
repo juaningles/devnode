7bcdeded0bce807a3d0d523ba179b3c205b8bd39
Juan Ingles <juan@korra.local>
2026-01-03 23:01:33 -0800

    new install method supports 18.04 - 26.04

--- ubuntu_installs/install_odbc.sh
+++ ubuntu_installs/install_odbc.sh
@@ -6,20 +6,20 @@ SUDO=$(which sudo |  head -n 1 | tr -d "\r\n")
 $SUDO apt-get update
 $SUDO apt-get -y install python3 python3-pip
 
-$SUDO apt-get -y install gnupg2
-$SUDO apt-get -y install apt-transport-https
-# previous method: curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# $SUDO apt-get -y install gnupg2
+# $SUDO apt-get -y install apt-transport-https
 
-##curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-##curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+# install the Microsoft ODBC driver for SQL Server
+# instructions as of 01/03/2025
+# https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver17&tabs=ubuntu18-install%2Cubuntu17-install%2Cdebian8-install%2Credhat7-13-install%2Crhel7-offline
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | sed -e 's/22\.10/22.04/' | sed -e 's/24\.../22.04/' | sed -e 's/26\.../25.10/')/packages-microsoft-prod.deb
+$SUDO dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | $SUDO gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
-curl https://packages.microsoft.com/keys/microsoft.asc |  $SUDO tee /etc/apt/trusted.gpg.d/microsoft.asc
-curl https://packages.microsoft.com/config/ubuntu/$(awk -F "=" '/^DISTRIB_RELEASE=/ {print $2}' /etc/lsb-release | $SUDO sed -e 's/22\.10/22.04/' | $SUDO sed -e 's/24\.04/22.04/')/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 $SUDO apt-get update
 export ACCEPT_EULA=Y
-$SUDO apt-get -y install msodbcsql18
+# $SUDO apt-get -y install msodbcsql18
 
 # optional: for bcp and sqlcmd
 $SUDO apt-get install -y mssql-tools18
