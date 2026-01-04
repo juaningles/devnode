1c63e9a643cf9a6314c02a8dbee2b596bc8c68e1
Juan Ingles <juan@korra.local>
2026-01-04 07:25:22 -0800

    change from apt to tar method

--- ubuntu_installs/install_trivy.sh
+++ ubuntu_installs/install_trivy.sh
@@ -4,8 +4,8 @@ set -e -x
 
 SUDO=$(which sudo |  head -n 1 | tr -d "\r\n")
 $SUDO apt-get update
-$SUDO apt-get install -y wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | $SUDO tee /usr/share/keyrings/trivy.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | $SUDO tee -a /etc/apt/sources.list.d/trivy.list
-$SUDO apt-get update
-$SUDO apt-get install -y trivy
+$SUDO apt-get install -y wget
+TRIVY_VERSION=$(curl -s "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+wget -qO trivy.tar.gz https://github.com/aquasecurity/trivy/releases/latest/download/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz
+$SUDO tar xf trivy.tar.gz -C /usr/local/bin trivy
+rm -rf trivy.tar.gz
